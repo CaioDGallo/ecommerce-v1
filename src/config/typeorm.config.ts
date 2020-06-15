931ef8bfddcdc05ee611b1ee99ebc84b1d4421d7
Caio Delafiori Gallo <caiogallo88@gmail.com>
2020-06-14 22:46:52 -0300

    Testing mongodb connection

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'mongodb',
-    url: process.env.MONGOURL,
+    url: 'mongodb+srv://caiogallo:forthehorde2401@cluster0-evtam.mongodb.net/ecommerce',
     synchronize: true,
     useUnifiedTopology: true,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
