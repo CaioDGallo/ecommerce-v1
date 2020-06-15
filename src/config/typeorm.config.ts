cb46b1dd39e70f89ce0028e7bf008e33576929fa
Caio Delafiori Gallo <caiogallo88@gmail.com>
2020-06-15 17:40:46 -0300

    Changing hardcoded url to environment variable

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'mongodb',
-    url: 'mongodb+srv://caiogallo:forthehorde2401@cluster0-evtam.mongodb.net/ecommerce',
+    url: process.env.MONGOURL,
     synchronize: true,
     useUnifiedTopology: true,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
